92e4a24dc3352b4d86155327ec01c98ccbd1c4ab
Sabir Izrafilov <SubBeer@gmail.com>
2015-05-25 18:16:29 +0300

    using utils/config to load configuration file

--- src/detokenizer-auxiliary.cpp
+++ src/detokenizer-auxiliary.cpp
@@ -44,6 +44,10 @@ int aux_init (const char *p_pszSettings)
 		return -4;
 	}
 
+	if (0 == iRetVal) {
+		g_coLog.WriteLog ("%s: mms detokenizer module is initialized successfully", __FUNCTION__);
+	}
+
 	return iRetVal;
 }
 
@@ -202,53 +206,5 @@ int detokenizer_apply_settings (
 	const char *p_pszSettings,
 	CConfig &p_coConfig)
 {
-	int iRetVal = 0;
-	int iFnRes;
-	const char *pszParamName;
-	char *pszNextParam;
-	char *pszParamVal;
-	unsigned int uiParamMask = 0;
-
-	pszParamName = p_pszSettings;
-	std::string strParamVal;
-
-	while (pszParamName) {
-		/* запоминаем следующий параметр */
-		pszNextParam = (char *) strstr (pszParamName, ";");
-		if (pszNextParam) {
-			*pszNextParam = '\0';
-			++pszNextParam;
-		}
-		/* получаем указатель на значение параметра */
-		pszParamVal = (char *) strstr (pszParamName, "=");
-		if (! pszParamVal) {
-			goto mk_continue;
-		}
-		*pszParamVal = '\0';
-		++pszParamVal;
-
-		/* контроль наличия всех необходимых параметров */
-		if (0 == strcmp ("db_user", pszParamName)) {
-			uiParamMask |= 1;
-		} else if (0 == strcmp ("db_pswd", pszParamName)) {
-			uiParamMask |= 2;
-		} else  if (0 == strcmp ("db_descr", pszParamName)) {
-			uiParamMask |= 4;
-		} else  if (0 == strcmp ("log_file_mask", pszParamName)) {
-			uiParamMask |= 8;
-		}
-		strParamVal = pszParamVal;
-		p_coConfig.SetParamValue (pszParamName, strParamVal);
-
-mk_continue:
-		/* переходим к следующему параметру */
-		pszParamName = pszNextParam;
-	}
-
-	/* проверяем, все ли нужные параметры мы получили */
-	if (! (uiParamMask & (unsigned int) (16 - 1))) {
-		iRetVal = uiParamMask;
-	}
-
-	return iRetVal;
+	return p_coConfig.LoadConf(p_pszSettings, 0);
 }
